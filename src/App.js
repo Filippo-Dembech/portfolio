986568929b62d7e5ecf4f4425505a5ce9b05b824
Filippo Dembech <filippo.dembech@gmail.com>
2025-05-08 18:26:42 +0200

    feat: typewriter typing effect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { TypeAnimation } from "react-type-animation";
+import Typewriter from "./Typewriter";
+
 function App() {
-  return (
-    <div>
-    </div>
-  );
+
+    return (
+        <div className="bg-orange-100">
+            <h1>
+            <TypeAnimation
+              sequence={[
+                "Hi there,\nI'm Phil.\nWelcome to my world!",
+              ]}
+              className="font-bold whitespace-pre-line font-custom-bebas text-orange-600 text-5xl"
+              wrapper="span"
+              speed={1}
+              style={{ fontSize: '2em', display: 'inline-block' }}
+              repeat={Infinity}
+            />  
+            </h1>
+            <Typewriter keySequence="Hi there, I'm Phil. Welcome to my world!"/>
+        </div>
+    );
 }
 
 export default App;
