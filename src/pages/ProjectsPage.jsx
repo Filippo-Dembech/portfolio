9fd66163e870b1485097828151d56a1516a180e3
Filippo Dembech <filippo.dembech@gmail.com>
2025-06-25 11:05:13 +0200

    refactor: make project descriptions better

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -6,14 +6,15 @@ const projects = [
     {
         title: "Red Cross Crammer",
         description:
-            "A simple webpage designed to help Red Cross students memorizing procedures.",
+            "An interactive study aid designed for Red Cross volunteers. It helps quickly memorizing emergency procedures through a clean, focused interface. Perfect for brushing up on protocols.",
         img: "./cri-crammer-img.png",
         projectLink: "https://filippo-dembech.github.io/cri-trainer/",
         codeLink: "https://github.com/Filippo-Dembech/cri-trainer",
     },
     {
         title: "Tic Tac Toe",
-        description: "Play tic-tac-toe with a friend of yours.",
+        description:
+            "A classic game of tic-tac-toe you can play in the browser with a friend. it's a fun little project that showcases basic game logic and player interactions.",
         img: "./tic-tac-toe-img.png",
         projectLink: "https://filippo-dembech.github.io/tic-tac-toe/",
         codeLink: "https://github.com/Filippo-Dembech/tic-tac-toe",
@@ -21,7 +22,7 @@ const projects = [
     {
         title: "Spanflex",
         description:
-            "A memorization game I have created to improve memory span.",
+            "A brain-training game designed to challenge and expand your short-term memory. Spanflex presents an increasingly difficult sequence of numbers, testing how far you can stretch your memory span.",
         img: "spanflex-img.png",
         projectLink: "https://filippo-dembech.github.io/Spanflex/",
         codeLink: "https://github.com/Filippo-Dembech/Spanflex",
@@ -29,13 +30,13 @@ const projects = [
     {
         title: "Powershell Prompt Profile",
         description:
-            "A Powershell profile that handles navigation and programs executions I made to make OS operations faster.",
+            "A custom PowerShell profile I built to streamline terminal navigation, automate repetitive tasks, and speed up everyday OS operations.",
         codeLink: "https://github.com/Filippo-Dembech/MyProfile",
     },
     {
         title: "Usefiltex",
         description:
-            "An npm React library created to filter list of objects with a declarative approach.",
+            "A lightweight and flexible React hook for filtering object lists in a declarative way. Available as an npm package it helps the client code to filter records, cutting down on all the filter() and map() methods.",
         projectLink: "https://www.npmjs.com/package/usefiltex",
         codeLink: "https://github.com/Filippo-Dembech/useFilter",
     },
@@ -49,7 +50,6 @@ export default function ProjectsPage() {
                 <div className="flex flex-col odd:bg-orange-200 even:bg-orange-50">
                     {projects.map((project) => (
                         <ProjectSection
-                            className="odd:bg-orange-200 even:bg-orange-50 md:odd:flex-row md:even:flex-row-reverse md:items-center md:gap-20"
                             title={project.title}
                             description={project.description}
                             img={project.img}
