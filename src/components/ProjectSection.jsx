a0def0b3f0282f7f72a0e1f34cdb9ae8fe0f738a
Filippo Dembech <filippo.dembech@gmail.com>
2025-06-25 10:56:28 +0200

    refactor: ProjectSection

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -1,22 +1,47 @@
 import ExternalLink from "./ExternalLink";
 import Title from "../components/Title";
 import InViewSlider from "../components/InViewSlider";
+import { classes } from "../utils/classes";
 
 export default function ProjectSection({
     title,
     description,
     img,
-    className,
     projectLink,
     codeLink,
 }) {
+
     const renderProjectLink = () => (
         <ExternalLink href={projectLink}>View Project</ExternalLink>
     );
+    
+
+    const renderImg = () => (
+        <div className="relative mb-3 sm:self-center md:flex-1/2">
+            <img
+                src={img}
+                className="rounded-xl"
+                alt="project-img"
+            />
+        </div>
+    );
+
+    const sliderClassName = classes(
+        "flex",
+        "flex-col",
+        "p-7",
+        "gap-8",
+        "sm:p-15",
+        "odd:bg-orange-200",
+        "even:bg-orange-50",
+        "md:odd:flex-row",
+        "md:even:flex-row-reverse",
+        "md:items-center",
+        "md:gap-20"
+    );
+
     return (
-        <InViewSlider
-            className={`flex flex-col p-7 gap-8 sm:p-15 ${className}`}
-        >
+        <InViewSlider className={sliderClassName}>
             <div className="flex flex-col gap-5 md:flex-1/2">
                 <Title
                     h={2}
@@ -25,25 +50,12 @@ export default function ProjectSection({
                     {title}
                 </Title>
                 <p className="font-custom-tuffy">{description}</p>
-                <div className="">
+                <div>
                     {projectLink && renderProjectLink()}
-                    <ExternalLink
-                        className="rounded-lg bg-orange-500 text-orange-50 font-bold p-3 hover:bg-orange-400"
-                        href={codeLink}
-                    >
-                        View Code
-                    </ExternalLink>
+                    <ExternalLink href={codeLink}>View Code</ExternalLink>
                 </div>
             </div>
-            {img && (
-                <div className="relative mb-3 sm:self-center md:flex-1/2">
-                    <img
-                        src={img}
-                        className="rounded-xl"
-                        alt="project-img"
-                    />
-                </div>
-            )}
+            {img && renderImg()}
         </InViewSlider>
     );
 }
