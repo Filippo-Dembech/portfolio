df3f3a9a31f70e297000c6d05d7e98f65532cce1
Filippo Dembech <filippo.dembech@gmail.com>
2025-06-30 16:16:36 +0200

    style: fix tailwind downgrade classes

--- src/pages/Homepage/HomepageLink.jsx
+++ src/pages/Homepage/HomepageLink.jsx
@@ -1,13 +1,47 @@
 import LottieIcon from "../../components/LottieIcon";
 import Navlink from "../../components/Navlink";
+import { classes } from "../../utils/classes";
 
 export default function HomepageLink({ to, external, iconSrc, children, lottiClassName }) {
+    
+    const linkClassName = classes(
+        "group",
+        "sm:flex-1",
+        "bg-orange-500",
+        "text-orange-50",
+        "font-custom-bebas",
+        "rounded-2xl",
+        "p-3",
+        "text-3xl",
+        "relative",
+        "sm:text-center",
+        "sm:w-1/9",
+        "transition-all",
+        "duration-300",
+        "hover:scale-110",
+        "hover:-translate-y-3",
+        "hover:shadow-lg"
+    )
+    
+    const textClassName = classes(
+        "inline-block",
+        "transform",
+        "duration-200",
+        "text-blue-300",
+        "group-hover:rotate-[-10deg]",
+        "origin-bottom-left",
+        "group-hover:text-orange-200",
+        "group-hover:translate-x-5",
+        "sm:group-hover:-translate-x-10",
+        "group-hover:scale-150"
+    )
+
     return (
         <Navlink
             to={to}
             external={external}
-            linkClassName="group sm:flex-1 bg-orange-500 text-orange-50 font-custom-bebas rounded-2xl p-3 text-3xl relative sm:text-center sm:w-1/9 transition-all duration-300 hover:scale-110 hover:-translate-y-3 hover:shadow-lg"
-            textClassName="inline-block transform duration-200 group-hover:rotate-[-10deg] origin-bottom-left group-hover:text-orange-200 group-hover:translate-x-5 sm:group-hover:-translate-x-10 group-hover:scale-150"
+            linkClassName={linkClassName}
+            textClassName={textClassName}
             sideElement={
                 <LottieIcon
                     className={lottiClassName}
--- src/pages/Homepage/linksData.js
+++ src/pages/Homepage/linksData.js
@@ -1,31 +1,77 @@
+import { classes} from '../../utils/classes'
+
+const projectsClassName = classes(
+    "absolute",
+    "top-1/2",
+    "-translate-y-1/2",
+    "right-[-50px]",
+    "max-w-[350px]",
+    "sm:right-0",
+    "sm:scale-[4]",
+    "mix-blend-darken",
+)
+
+const articlesClassName = classes(
+    "absolute",
+    "top-1/2",
+    "-translate-y-1/2",
+    "right-4",
+    "w-40",
+    "sm:right-1/2",
+    "sm:translate-x-1/2",
+    "sm:scale-[1.2]",
+    "mix-blend-luminosity",
+)
+
+const storyClassName = classes(
+    "absolute",
+    "top-1/2",
+    "-translate-y-1/2",
+    "right-[-20px]",
+    "w-1/2",
+    "max-w-[200px]",
+    "rotate-[20deg]",
+    "sm:right-0",
+    "sm:scale-[4]",
+    "mix-blend-darken",
+    "opacity-85", 
+)
+
+const contactsClassName = classes(
+    "absolute",
+    "top-1/2",
+    "-translate-y-1/2",
+    "right-4",
+    "w-3/5",
+    "max-w-[200px]",
+    "rotate-[-20deg]",
+    "sm:scale-[3]",
+)
+
 const linksData =  [
     {
         title: "My Projects",
         to: "/projects",
         iconSrc: process.env.PUBLIC_URL + "/projects.lottie",
-        lottiClassName:
-            "absolute top-1/2 -translate-y-1/2 right-[-50px] w-full max-w-[350px] sm:right-0 sm:scale-400 mix-blend-darken",
+        lottiClassName: projectsClassName
     },
     {
         title: "My Articles",
         external: "https://filippo-dembech.github.io/articles/",
         iconSrc: process.env.PUBLIC_URL + "/articles.lottie",
-        lottiClassName:
-            "absolute top-1/2 -translate-y-1/2 right-4 w-40 sm:right-1/2 sm:translate-x-1/2 sm:scale-120 mix-blend-luminosity",
+        lottiClassName: articlesClassName,
     },
     {
         title: "My Story",
         to: "/story",
         iconSrc: process.env.PUBLIC_URL + "/story.lottie",
-        lottiClassName:
-            "absolute top-1/2 -translate-y-1/2 right-[-20px] w-1/2 max-w-[200px] rotate-20 sm:right-0 sm:scale-400 sm:rotate-20 mix-blend-darken opacity-85",
+        lottiClassName: storyClassName,
     },
     {
         title: "My Contacts",
         to: "/contacts",
         iconSrc: process.env.PUBLIC_URL + "/contacts.lottie",
-        lottiClassName:
-            "absolute top-1/2 -translate-y-1/2 right-4 w-3/5 max-w-[200px] rotate-[-20deg] sm:scale-300",
+        lottiClassName: contactsClassName,
     },
 ];
 
