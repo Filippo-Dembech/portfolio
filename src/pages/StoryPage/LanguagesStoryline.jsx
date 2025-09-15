c412270df5428ecee05d880aefd3e9456a413675
Filippo Dembech <filippo.dembech@gmail.com>
2025-09-15 17:39:20 +0200

    add: images from languages storyline

--- src/pages/StoryPage/LanguagesStoryline.jsx
+++ src/pages/StoryPage/LanguagesStoryline.jsx
@@ -3,19 +3,27 @@ import {
     VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
-import languagesData from './languagesData';
-
+import languagesData from "./languagesData";
 
 export default function LanguagesStoryline() {
     return (
         <VerticalTimeline>
             {languagesData.map((language, i) => (
                 <VerticalTimelineElement
-                    class
                     className="vertical-timeline-element--work"
                     dateClassName="mx-2"
+                    icon={
+                        <div className="flex justify-center items-center">
+                            <img
+                                src={`${process.env.PUBLIC_URL}/${language.imgPath}`}
+                                alt="icon-img"
+                                width="80%"
+                            />
+                        </div>
+                    }
                     date={language.date}
-                    iconClassName={language.iconClassName}
+                    iconClassName={`bg-white flex ${language.iconClassName}`}
+                    //iconClassName={`flex ${language.iconClassName}`}
                 >
                     <h3 className="text-[2rem]">{language.name}</h3>
                     <p className="font-light">{language.description}</p>
--- src/pages/StoryPage/languagesData.js
+++ src/pages/StoryPage/languagesData.js
@@ -2,65 +2,78 @@ const languagesData = [
     {
         name: "VBA",
         date: "2019",
-        iconClassName: "bg-orange-100",
         description: "The first language I have ever learnt",
+        imgPath: "vba.png"
     },
     {
         name: "HTML, CSS, JS",
         date: "2020 - present",
-        iconClassName: "bg-orange-200",
         description: "From VBA interfaces to Web Interfaces...",
+        imgPath: "web-logo.png",
     },
     {
         name: "Python",
         date: "2021/2022",
-        iconClassName: "bg-orange-300",
         description:
             "From now on, programming becomes a passion beyond interfaces...",
+        imgPath: "python.png"
     },
     {
         name: "C",
         date: "2022",
-        iconClassName: "bg-orange-400",
         description: "I wanted to learn where all comes from...",
+        imgPath: "c.png",
     },
     {
         name: "Java",
         date: "2021/2023",
-        iconClassName: "bg-orange-500",
         description:
             "Welcome to OOP, mate! Learn how to use types, data structures, how to design flexible code",
+        imgPath: "java.png",
     },
     {
         name: "Typescript",
         date: "2023 - present",
-        iconClassName: "bg-orange-600",
         description: "OOP for the Web??? Where do I have to sign?",
+        imgPath: "typescript.png"
     },
     {
         name: "Dart",
         date: "2023/2024",
-        iconClassName: "bg-orange-700",
         description:
             "I wanted to create mobile applications and went for a new language... again...",
+        imgPath: "dart.png"
     },
     {
         name: "Kotlin",
         date: "2024",
-        iconClassName: "bg-orange-800",
         description: "Just Android! No more iOS, sorry ladies",
+        imgPath: "kotlin.png"
     },
     {
         name: "Powershell",
         date: "2024",
-        iconClassName: "bg-orange-900",
         description: "I wanted to automize my Windows OS operations",
+        imgPath: "powershell.png"
     },
     {
-        name: "JSX",
+        name: "React / Next.js",
         date: "2024 - present",
-        iconClassName: "bg-red-800",
         description: "Take away from me everything, but not my components!",
+        imgPath: "react.png"
+    },
+    {
+        name: "PHP",
+        date: "2025",
+        description: "Had to face this facinating language to handle with old technologies.",
+        imgPath: "php.png"
+    },
+    {
+        name: "C++",
+        date: "2025",
+        description:
+            "I had to work with some Arduino projects and had to learn C++ better",
+        imgPath: "c++.png"
     },
 ];
 
